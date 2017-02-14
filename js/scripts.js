030d2c7a32596de9432bf6263b2237654b480986
Evan Bishop and Liam Stabeno <evan@>
2017-02-14 13:20:35 -0800

    add hover event handler

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,56 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".hiddenwalrus").slideToggle();
-    // $(".hiddenwalrus").slideToggle();
-  });
+  $(".clickable1").hover(function() {
+    $(".slide1").slideToggle();
+    });
+  $(".clickable2").hover(function() {
+    $(".slide2").slideToggle();
+    });
+  $(".clickable3").hover(function() {
+    $(".slide3").slideToggle();
+    });
+  $(".clickable4").hover(function() {
+    $(".slide4").slideToggle();
+    });
+  $(".clickable5").hover(function() {
+    $(".slide5").slideToggle();
+    });
+  $(".clickable6").hover(function() {
+    $(".slide6").slideToggle();
+    });
+  $(".clickable7").hover(function() {
+    $(".slide7").slideToggle();
+    });
+  $(".clickable8").hover(function() {
+    $(".slide8").slideToggle();
+    });
+  $(".clickable9").hover(function() {
+    $(".slide9").slideToggle();
+    });
+  $(".clickable10").hover(function() {
+    $(".slide10").slideToggle();
+    });
+  $(".clickable11").hover(function() {
+    $(".slide11").slideToggle();
+    });
+  $(".clickable12").hover(function() {
+    $(".slide12").slideToggle();
+    });
+  $(".clickable13").hover(function() {
+    $(".slide13").slideToggle();
+    });
+  $(".clickable14").hover(function() {
+    $(".slide14").slideToggle();
+    });
+  $(".clickable15").hover(function() {
+    $(".slide15").slideToggle();
+    });
+  $(".clickable16").hover(function() {
+    $(".slide16").slideToggle();
+    });
+  $(".clickable17").hover(function() {
+    $(".slide17").slideToggle();
+    });
+  $(".clickable18").hover(function() {
+    $(".slide18").slideToggle();
+    });
 });
